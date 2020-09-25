9af3d716bbd7c4817b601c8041b4ce7bc7a10c5d
Ramūnas <ramunas.daukas@gmail.com>
2020-09-25 08:22:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+     EditText editBox;
+     TextView showData;
+     Counting textAnalyzis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.showData = findViewById(R.id.textDisplayData);
+        this.editBox = findViewById(R.id.textInputBox);
+    }
+
+    public void onBtnCharClick(View view) {
+        int count;
+        count = textAnalyzis.countChar(this.editBox.getText());
+        showData.setText("Number of chars: " + count);
+    }
+
+    public void onBtnWordClick(View view) {
+        int count = textAnalyzis.countWords(this.editBox.getText().toString());
+        showData.setText("Number of Words: " + count);
+    }
+}
+class Counting{
+    int countChar(Editable words){
+
+        return words.length();
+    }
+    int countWords(String words){
+
+        return Arrays.toString(words.split("\\s|(?<!\\d)[,.](?!\\d)")).length();
+
+    }
+}
