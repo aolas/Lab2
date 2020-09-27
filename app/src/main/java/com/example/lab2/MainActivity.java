d6078e8d9890c3868d6abf9f9432abab85030db3
Ramunas Daukas <ramunas.daukas@gmail.com>
2020-09-27 12:02:40 +0300

    Added Toast

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
      TextView showData;
      Counting textAnalyzis;
     Spinner spinner;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,37 +27,33 @@ public class MainActivity extends AppCompatActivity {
         textAnalyzis = new Counting();
         this.showData = findViewById(R.id.textDisplayData);
         this.editBox = findViewById(R.id.textInputBox);
+        toast =Toast.makeText(getApplicationContext(),R.string.empty,Toast.LENGTH_SHORT);
+        toast.setMargin(50,50);
         this.spinner = findViewById(R.id.spnSelect);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countActions, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
 
-    public void onBtnCharClick(View view) {
-        int count;
-        count = textAnalyzis.countChar(this.editBox.getText());
-        showData.setText(R.string.number_chars + count);
-    }
-
-    public void onBtnWordClick(View view) {
-        int count = textAnalyzis.countWords(this.editBox.getText().toString());
-        showData.setText(R.string.number_Words + count);
-    }
     public void onBtnClick(View view){
         int position = spinner.getSelectedItemPosition();
         int count;
         String message;
         String output = String.format( getString(R.string.number_Words) ,  35);
 
-        if (position == 0){
-            count = textAnalyzis.countWords(this.editBox.getText().toString());
-            //showData.setText(R.string.number_Words + count);
-            message = String.format(getString(R.string.number_Words), count);
-            showData.setText(message);
-        }else {
-            count = textAnalyzis.countChar(this.editBox.getText());
-            message = String.format(getString(R.string.number_chars), count);
-            showData.setText(message);
+        if (this.editBox.getText().toString().length() > 0) {
+            if (position == 0) {
+                count = textAnalyzis.countWords(this.editBox.getText().toString());
+                message = String.format(getString(R.string.number_Words), count);
+                showData.setText(message);
+            } else {
+                count = textAnalyzis.countChar(this.editBox.getText());
+                message = String.format(getString(R.string.number_chars), count);
+                showData.setText(message);
+            }
+        } else{
+            showData.setText(R.string.information);
+            toast.show();
         }
     }
 }
